7f92407a19479ba05a888dd735728590cd971bf0
Kenichi-BOTZ <106317066+Kenichi-BOTZ@users.noreply.github.com>
2023-01-01 14:54:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,21 @@
 global.DeveloperMode = 'true' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['-']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62895388375000': { // put your number here
-    name: 'Owner Kenichi/Creator KeniBotz',
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: true // if true this number will send if user use command /owner or /creator
-  },
-  '6285893400504': { // put your number here
+  '6285810791701': { // put your number here
+    name: 'Yusup',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
-  },
-  '62895388375000': { // put your number here
-    name: 'Dave',
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '62895388375000': {
-    isReport: true // if true this number will send if user use command /report or /join
+  '6281334567203': { // put your number here
+    isDev: true, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['6285810791701'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -55,10 +46,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'KeniBotz Sticker'
-global.author = 'Kenichi ~ ILY ♥️😗'
+global.packname = 'YusupBot'
+global.author = 'Yusup'
 
-global.multiplier = 39 // The higher, The harder levelup
+global.multiplier = 50 // The higher, The harder levelup
 
 let fs = require('fs')
 let chalk = require('chalk')
